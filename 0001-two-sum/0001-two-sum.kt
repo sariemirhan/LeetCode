4fb3531a5f36e75bc0251417a96d2499b54abc50
Emirhan Sarı <69917349+sariemirhan@users.noreply.github.com>
2024-05-30 23:44:52 +0300

    Time: 184 ms (94.07%), Space: 38.5 MB (36.34%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val map = mutableMapOf<Int,Int>()
+        for ((index,value) in nums.withIndex()){
+
+            val complement = target - value 
+            
+            if (map.containsKey(complement)) {
+            
+                return intArrayOf(map[complement]!!,index)
+            
+            }
+            
+            map[value]= index
+        }
+        throw IllegalArgumentException("No two sum solution")
+}}//edited for sync my github
